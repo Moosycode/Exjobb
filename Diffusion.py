a10403318e28e83a32e4df97be32a236ff84244c
Nils Wikström <nils.wikstrom@outlook.com>
2024-02-26 16:14:35 +0100

    changes 20240226

--- Diffusion.py
+++ Diffusion.py
@@ -3,17 +3,23 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 # Parameters
-L = 3 # Length in micrometer
-T = 500  # Total time in seconds
+L = 3 # Length of sample [micrometer]
+T = 30000  # Total time [seconds]
 Nx = 100  # Number of spatial points
-Nt = 500 # Number of time steps
-D0 = 2e-4  # Diffusion coefficient inital value
-Ea = 2.5 #Activation energy for diffusion in eV
+Nt = 3000 # Number of time steps
+D0 = 5.3e-3  # Diffusion coefficient inital value [cm^2/s]
+Ea = 1.08 #Activation energy for diffusion in kJ/mole or eV depending on choise of k
 dx = L / (Nx - 1) # Spatial step size
 dt = T / Nt # Time step size
-k = 8.6e-5 # boltzmann constant in eV/K
-Temp = 2000 #Temperature in K
+#k = 8.3e-3 # boltzmann constant [kJ/(mol*K)]
+k = 8.6e-5 # boltzmann constant [ev/K]
+Temp = 2000 #Temperature [K]
 root = '/Users/niwi9751/Srim_Results/Fe_in_ZrO2.txt'
+
+rho = 5.68 # density of target in g/cm^3
+m_a = 123.218 # molar mass of target in g/mole
+Na = 6.022e22 # avogadros number
+n = rho*Na/m_a
 
 #/Users/niwi9751/Srim_Results/Fe_in_ZrO2.txt
 def  read_columns(root):
@@ -31,7 +37,7 @@ def  read_columns(root):
 # Diffusion coefficient dependant on temperature
 def D(D0, Ea, Temp):
     D = D0*np.exp(-Ea/(k*Temp))
-    return D0
+    return D
 
 # Gaussian
 def gaussian(x,amp,mu,sigma):
@@ -43,7 +49,8 @@ def fit_gauss(data, plot = False):
     x = np.linspace(min(data), max(data),60)
     x = x*0.0001 #rescale to micrometer
     y = n
-    y = 100*y/len(data) #rescale to atomic %
+    #y = 100*y/len(data) #rescale to atomic %
+    y = n/0.11 #Normalize
     popt, pcov = curve_fit(gaussian,x,y)
     if plot:
         plt.plot(x,gaussian(x,*popt))
@@ -73,12 +80,16 @@ for n in range(0, Nt - 1):
 
 # Plot the results
 plt.figure(figsize=(8, 6))
-for n in range(0, Nt, Nt//10):
-    plt.plot(x, C[n, :], label=f"t={n*dt:.2f}")
-
+# for n in range(0, Nt, Nt//10):
+#     plt.plot(x, C[n, :], label=f"t={n*dt:.2f}")
+plt.plot(x,C[0,:], label = 'Initial distribution')
+plt.plot(x,C[-1,:], label = 'Final distribution')
 plt.title('Diffusion of Concentration')
 plt.xlabel('Position [micrometer]')
-plt.ylabel('Concentration [At. %]')
+plt.ylabel('Concentration [(atoms/cm^3)/(atoms/cm^2)]')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+
+
